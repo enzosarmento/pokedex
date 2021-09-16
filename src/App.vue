6de293706d42e69d59f693cd868899524dbf5d97
enzosarmento <enzosarmento91@gmail.com>
2021-09-16 13:09:55 -0300

    feat: pokédex layout

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <Navbar />
+  <router-view />
 </template>
 
+
+<script>
+import Navbar from "./components/Navbar.vue";
+
+export default {
+  name: "App",
+  components: {
+    Navbar,
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
+:root {
+  font-size: 62.5%;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+body {
+  font-family: "Poppins", sans-serif;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.container {
+  margin-left: 2.4rem;
+  margin-right: 2.4rem;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,59 @@
+<template>
+    <header>
+        <nav class="navbar container">
+            <div class="logo">
+                <img :src="pokeballNav" alt="Icon Pokeball">
+                <h1>Pokédex</h1> 
+            </div>
+            <div class="arrow">
+                <span>#</span>
+                <img :src="arrowDown" alt="Flecha para baixo">
+            </div>
+            
+        </nav>
+    </header>
+
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+      return {
+          pokeballNav: 'images/pokeball_nav.svg',
+          arrowDown: 'images/arrow_down.svg'
+      }
+  }
+};
+</script>
+
+<style scoped>
+    .navbar {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        padding-top: 2.4rem;
+        padding-bottom: 1.2rem;
+    }
+
+    .logo {
+        display: flex;
+        align-items: center;
+    }
+
+    .logo h1 {
+        font-size: 2.4rem;
+        margin-left: 1.6rem;
+    }
+
+    .arrow {
+        display: flex;
+        align-items: center;
+    }
+
+    .arrow span {
+        font-size: 1rem;
+        margin-right: .2rem;
+    }
+</style>
